1495bbac3c9da86a4ebf464ec774b35ae36ed47a
E. Aleyna Tanrıöver <a.aleynaelmas@gmail.com>
2025-09-23 22:34:05 +0300

    feat: add GameSignals for score, moves, busy and gameover events

---
+++ Assets/Scripts/Core/Signals/GameSignals.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace LinkMatch.Core.Signals
+{
+    public static class GameSignals
+    {
+        public static event Action<int> OnScoreChanged;
+        public static void ScoreChanged(int v) => OnScoreChanged?.Invoke(v);
+        
+        public static event Action<int> OnMovesChanged;
+        public static void MovesChanged(int v) => OnMovesChanged?.Invoke(v);
+
+        public static event Action<bool> OnBoardBusy;
+        public static void BoardBusy(bool v) => OnBoardBusy?.Invoke(v);
+
+        public static event Action<bool> OnGameOver;
+        public static void GameOver(bool win) => OnGameOver?.Invoke(win);
+    }
+}
