e6355a3077e8f9d9d8a6860c13458e93678eb363
E. Aleyna Tanrıöver <a.aleynaelmas@gmail.com>
2025-09-24 12:48:22 +0300

    feat: add GameOverPanel with win/lose messaging and replay button

---
+++ Assets/Scripts/Game/UI/GameOverPanel.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using LinkMatch.Core.Signals;
+
+namespace LinkMatch.Game.UI
+{
+    public sealed class GameOverPanel : MonoBehaviour
+    {
+        [SerializeField] private GameObject root;
+        [SerializeField] private TMP_Text title;
+        [SerializeField] private Button replayButton;
+
+        private void Awake()
+        {
+            if (root) root.SetActive(false);
+            if (replayButton) replayButton.onClick.AddListener(() =>
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex));
+        }
+
+        private void OnEnable(){ GameSignals.OnGameOver += OnGameOver; }
+        private void OnDisable(){ GameSignals.OnGameOver -= OnGameOver; }
+
+        private void OnGameOver(bool win)
+        {
+            if (root) root.SetActive(true);
+            if (title) title.text = win ? "You Win!" : "Try Again";
+        }
+    }
+}
